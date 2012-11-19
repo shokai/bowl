14636126a43cafe7c42752ae53a5411d8edd39ae
Sho Hashimoto <hashimoto@shokai.org>
2012-11-20 07:44:24 +0900

    show sample code with script tag  #4

--- helpers/plugins.rb
+++ helpers/plugins.rb
@@ -1,4 +1,18 @@
 
+helpers do
+  def plugin_sample_code(plugin)
+    post_data = {};
+    plugin.fields.each do |name,field|
+      post_data[name] = field[:default]
+    end
+
+    ["<script src=\"#{fishbowl_js}\">",
+     "<script type=\"text/javascript\">",
+     "  fishbowl.plugin(\"#{plugin.name}\", #{post_data.to_json});",
+     "</script>"].join("\n")
+  end
+end
+
 def plugins
   @@plugins ||=
     (
